ac91621af95af6b847b35778788fccf3906b45c2
tjordan4 <tysoncjordan@gmail.com>
2024-02-22 12:40:22 -0700

    added fetch

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -4,9 +4,28 @@ export default function (SignUpForm) {
 const [username, setUsername] = useState("")
 const [password, setPassword] = useState("")
 const [error, setError] = useState(null)
-   return <>
+   
+async function handleSubmit(event){
+    event.preventDefault();
+
+    try{
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                username: username.data,
+                password: password.data,
+            })
+        }) 
+    }  catch(error) {
+            setError(error.message)
+    }
+}   
+
+return <>
    <h2>Sign Up!</h2>
-    <form>
+   {error && <p>{error}</p>}
+    <form onSubmit={handleSubmit}>
         <label htmlFor="">
         Username: <input type="text" value={username} onChange={(event) =>
         setUsername(event.target.value)}/>
@@ -17,5 +36,6 @@ const [error, setError] = useState(null)
         </label>
         <button type="submit">Submit</button>
     </form>
+
     </>
 } 
